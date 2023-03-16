21ef9374d9cf21408802c3cad4ae8f01a28eea00
Nicolás Valor <nicoo.valor@gmail.com>
2023-03-16 09:56:06 -0300

    feat(cards container): agrego estilos de maquetado

--- Client/src/components/CardsContainer/CardsContainer.jsx
+++ Client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,44 +1,45 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getPizzas } from "../../redux/actions"
-import Card from "../Card/Card"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPizzas } from "../../redux/actions";
+import Card from "../Card/Card";
+import styles from "./CardsContainer.module.css";
 
 const CardsContainer = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const pizzas = useSelector((state) => state.pizzas);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const pizzas = useSelector(state => state.pizzas)
-    const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    dispatch(getPizzas());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getPizzas())
-    }, [dispatch])
+  useEffect(() => {
+    pizzas.length && setIsLoading(false);
+  }, [pizzas]);
 
-    useEffect(() => {
-        pizzas.length && setIsLoading(false)
-    }, [pizzas])
-    
-    return (
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
         <>
-            {
-                isLoading ? <p>Loading...</p> :
-                    <>
-                       <div>
-                        {
-                            pizzas.map(pizza => {
-                                return <Card
-                                    key={pizza.id}
-                                    id={pizza.id}
-                                    name={pizza.name}
-                                    image={pizza.image}
-                                />
-                            })
-                        }
-                       </div>
-                    </>
-            }
+          <div className={styles.container}>
+            {pizzas.map((pizza) => {
+              return (
+                <Card
+                  key={pizza.id}
+                  id={pizza.id}
+                  name={pizza.name}
+                  image={pizza.image}
+                />
+              );
+            })}
+          </div>
         </>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default CardsContainer
+export default CardsContainer;
