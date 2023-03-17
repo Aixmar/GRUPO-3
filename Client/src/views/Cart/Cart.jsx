d1755910a49d334333c6e191066cb786720a0dd4
aixmar <aixamarianag@gmail.com>
2023-03-17 00:05:10 -0300

    style(Cart): saqué un componente que no esta listo

--- Client/src/views/Cart/Cart.jsx
+++ Client/src/views/Cart/Cart.jsx
@@ -1,12 +1,12 @@
 import Table from "./Helper/Table/Table";
-import CartExtras from "./Helper/CartExtras/CartExtras";
+// import CartExtras from "./Helper/CartExtras/CartExtras";
 import CartFooter from "./Helper/CartFooter/CartFooter";
 
 const Cart = () => {
   return (
     <div>
       <Table />
-      <CartExtras />
+      {/* <CartExtras /> */}
       <CartFooter width="100%"/>
     </div>
   );
