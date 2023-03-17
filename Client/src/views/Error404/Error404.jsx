480033b1428308f82cfbbe711cb92e43e83a816c
Pablo Rossetti <prensap.ross@gmail.com>
2023-03-17 12:44:22 -0300

    fix error404

--- Client/src/views/Error404/Error404.jsx
+++ Client/src/views/Error404/Error404.jsx
@@ -1,9 +1,16 @@
+import { Box, Heading, Text, Link, Button } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import Footer from "../../components/Footer/Footer"
+
 const Error404 = () => {
   return (
-    <div>
-      <h3>Error404</h3>
-      <p>Page not found</p>
-    </div>
+    <Box textAlign="center" bgGradient="linear(to-l,#000000, #272727)" h="100vh" d="flex" alignItems="center" justifyContent="center">
+      <Box p={10} bgGradient="linear-gradient(to right, #f27825, #eab830)" borderRadius="lg" boxShadow="lg">
+        <Heading fontSize="6xl" fontWeight="bold" color="#272727" mb={4}>Error404</Heading>
+        <Text fontSize="2xl" color="white">Page not found</Text>
+        <Button as={RouterLink} to="/home" size="lg" mt={8} colorScheme="whiteAlpha">Go back to home</Button>
+      </Box>
+    </Box>
   );
 };
 
