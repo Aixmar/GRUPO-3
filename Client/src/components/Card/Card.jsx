e1d5cce91658400911103b5f66b7e66ca2c9d8fb
Pablo Rossetti <prensap.ross@gmail.com>
2023-03-18 19:36:44 -0300

    fix allpizass

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,22 +1,41 @@
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import styles from "./Card.module.css";
+import { Box, Flex, Grid, Image, Text } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/button";
 import { pushToCart } from "../../redux/actions";
 
 const Card = (props) => {
+  const dispatch = useDispatch();
 
+  const handleAddToCart = () => {
+    dispatch(pushToCart(props.id));
+  };
 
   return (
-    <div className={styles.container}>
+    <Box
+      bgGradient="linear-gradient(to right, #f27825, #eab830)"
+      borderRadius="md"
+      boxShadow="md"
+      overflow="hidden"
+      maxW="sm"
+    >
       <Link to={`/pizzadetail/${props.id}`}>
-        <div >
-          <h1>{props.name}</h1>
-          <img src={props.image} alt="" className={styles.image}></img>
-          <h1>${props.price}</h1>
-        </div>
+        <Image src={props.image} alt={props.name} w="full" h="300px" objectFit="cover" />
       </Link>
-    </div>
+      <Grid templateColumns="1fr" templateRows="1fr 50px" p="4">
+        <Text fontSize="lg" fontWeight="semibold" color="white">
+          {props.name}
+        </Text>
+        <Flex justifyContent="space-between" alignItems="center" pt="10px">
+          <Text fontSize="2xl" fontWeight="bold" color="#272727" mr="2">
+            ${props.price}
+          </Text>
+          <Button colorScheme="red" onClick={handleAddToCart}>
+            Add to cart
+          </Button>
+        </Flex>
+      </Grid>
+    </Box>
   );
 };
 
