6eb420a614ca728ca04d41585b03e5e06633c5a2
Pablo Rossetti <prensap.ross@gmail.com>
2023-03-18 16:12:34 -0300

    fix structure allpizzas

--- Client/src/components/Sorters/SortSelect.jsx
+++ Client/src/components/Sorters/SortSelect.jsx
@@ -1,24 +1,45 @@
+import { Select, FormControl, FormLabel } from "@chakra-ui/react";
+
+// Estados: Z-A, A-Z, Precio: Del mas bajo al mas alto, Precio: Del mas alto al mas bajo, Promedio opinion del cliente
+//   // States: Z-A, A-Z, Price: Low to high, Price: High to low, Avg. customers reviews
+
 const SortSelect = ({ Sort, selectedSort, setSelectedSort }) => {
-  // Estados: Z-A, A-Z, Precio: Del mas bajo al mas alto, Precio: Del mas alto al mas bajo, Promedio opinion del cliente
-  // States: Z-A, A-Z, Price: Low to high, Price: High to low, Avg. customers reviews
   const handleChange = (event) => {
     setSelectedSort(event.target.value);
   };
 
   return (
-    <div>
-      <label htmlFor="Sort-select">Sort by:</label>
-      <div>
-        <select id="Sort-select" value={selectedSort} onChange={handleChange}>
-          {Sort.map((Sort) => (
-            <option key={Sort} value={Sort}>
-              {Sort}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
+    <FormControl colorScheme="orange">
+      <FormLabel htmlFor="Sort-select" fontFamily="sans-serif" fontSize="xl" color="white">
+        Sort by:
+      </FormLabel>
+      <Select
+        id="Sort-select"
+        value={selectedSort}
+        onChange={handleChange}
+        color="#f27825"
+        borderColor="white"
+        borderWidth="2px"
+        borderRadius="full"
+        fontWeight="bold"
+        _focus={{
+          outline: "none",
+          borderColor: "white",
+          boxShadow: "none"
+        }}
+        _hover={{
+          borderColor: "#f27825"
+        }}
+      >
+        {Sort.map((Sort) => (
+          <option key={Sort} value={Sort}>
+            {Sort}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
   );
 };
 
 export default SortSelect;
+
--- Client/src/views/AllPizzas/AllPizzas.jsx
+++ Client/src/views/AllPizzas/AllPizzas.jsx
@@ -4,32 +4,35 @@ import SortSelect from "../../components/Sorters/SortSelect";
 import { useState } from "react";
 import Filters from "../../components/Filters/Filters";
 import DrinksContainer from "../../components/DrinksContainer/DrinksContainer";
-
 import { getPizzas } from "../../redux/actions";
+import { Box, Flex } from "@chakra-ui/react";
 
 const AllPizzas = () => {
   const dispatch = useDispatch();
   const [selectedSort, setSelectedSort] = useState("A-Z");
 
   return (
-    <>
+    <Flex bgGradient="linear(to-l,#000000, #272727)">
+      <Box flex="0 0 auto" mr="1rem">
+        <SortSelect
+          Sort={[
+            "A-Z",
+            "Z-A",
+            "Price: Low to high",
+            "Price: High to low",
+            "Avg. customers reviews",
+          ]}
+          selectedSort={selectedSort}
+          setSelectedSort={setSelectedSort}
+        />
+        <Filters />
+      </Box>
 
-      <SortSelect
-        Sort={[
-          "A-Z",
-          "Z-A",
-          "Price: Low to high",
-          "Price: High to low",
-          "Avg. customers reviews",
-        ]}
-        selectedSort={selectedSort}
-        setSelectedSort={setSelectedSort}
-      />
-
-      <Filters/>
-      <DrinksContainer/>
-      <CardsContainer selectedSort={selectedSort} />
-    </>
+      <Box flex="1 1 auto">
+        <DrinksContainer />
+        <CardsContainer selectedSort={selectedSort} />
+      </Box>
+    </Flex>
   );
 };
 
