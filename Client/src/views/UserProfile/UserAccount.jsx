9629e65055fe3a9395c370e93340158d6167de62
aixmar <aixamarianag@gmail.com>
2023-03-23 17:08:58 -0300

    build(UpdatePicture): add cloudinary userprofile

--- Client/src/views/UserProfile/UserAccount.jsx
+++ Client/src/views/UserProfile/UserAccount.jsx
@@ -1,11 +1,16 @@
 import user from "./json";
 import UserProfile from "./UserProfile";
-import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import { Box, useToast, Tooltip, Heading, Text, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import UpdateEmailForm from "./Updates/UpdateEmail";
 import UpdatePasswordForm from "./Updates/UpdatePassword";
 import { useState } from "react";
+import Axios from 'axios';
+import {updatePicture} from "./updatePicture";
 
 const UserAccount = () => {
+
+  const toast = useToast()
+
   const [isUpdateEmailFormVisible, setIsUpdateEmailFormVisible] =
     useState(false);
   const toggleUpdateEmailForm = () => {
@@ -24,9 +29,42 @@ const UserAccount = () => {
         <Heading as="h1" size="lg">
           Personal Info
         </Heading>
+
+
+        <Input 
+        id="inputTag"
+        type="file" 
+        onChange={ async (event) => {
+
+        toast({
+          position: 'top-left',
+          duration: 4000,
+          isClosable:true,
+          render: () => (
+            <Box color='white' p={3} bg='green.400' fontWeight="bold">
+              <Text>¡Cambiando foto! esto puede tardar varios segundos</Text>
+            </Box>
+          )
+          })
+
+        const formData = new FormData()
+        formData.append("file", event.target.files[0])
+        formData.append("upload_preset", "users_photo")
+
+
+        Axios.post("https://api.cloudinary.com/v1_1/dozwiqjh1/image/upload", formData)
+        .then(Response => updatePicture(user.email, Response.data.url))
+      
+        }}
+
+          />
+
+
+
         <Heading as="h2" size="md" marginTop="4">
           Name
         </Heading>
+
         <Text>{user[0].name}</Text>
         <Heading as="h2" size="md" marginTop="4">
           Lastname
@@ -45,7 +83,7 @@ const UserAccount = () => {
             Update
           </Button>
         </Text>
-        {isUpdateEmailFormVisible && <UpdateEmailForm formType="email" />}
+        {isUpdateEmailFormVisible && <UpdateEmailForm formtype="email" />}
 
         <Heading as="h2" size="md" marginTop="4">
           Password
