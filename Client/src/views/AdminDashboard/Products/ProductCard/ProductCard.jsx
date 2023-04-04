3f5c32f909a8a57c8d70b5b1343ef83f189c9784
MuraJoaquin <joaco_mura@hotmail.com>
2023-04-04 14:34:45 -0300

    agregue searchBar a dashboard prod

--- Client/src/views/AdminDashboard/Products/ProductCard/ProductCard.jsx
+++ Client/src/views/AdminDashboard/Products/ProductCard/ProductCard.jsx
@@ -18,12 +18,12 @@ const ProductCard = (props) => {
         .then()
     },[active])
     return (
-        <Flex padding="20px" border="1px solid white">
-            <Text marginRight={10} color="#f27825">{props.id}</Text>
-            <Image w="45px" src={props.image} marginRight={5} alt={props.name} />
-            <Text marginRight={10} color="white">{props.name}</Text>
-            <Text marginRight={5} color="white">${props.price}</Text>
-            <Text paddingLeft="10px" marginRight={5} color="white">{props.stock} prod.</Text>
+        <Flex padding="20px" border="1px solid white" justify="space-between" gap="6rem"> 
+            <Text  w="2rem" color="#f27825">{props.id}</Text>
+            <Image w="3rem" h="3rem" src={props.image} marginRight={5} alt={props.name} />
+            <Text  w="11rem" color="white" overflowWrap='anywhere' overflow= 'hidden' flexWrap='wrap' sx={{ WebkitLineClamp: [1, 2], WebkitBoxOrient: 'vertical', display: '-webkit-box' }}>{props.name}</Text>
+            <Text  w="5rem" color="white">${props.price}</Text>
+            {/* <Text paddingLeft="10px" marginRight={5} color="white">{props.stock} prod.</Text> */}
             {
                 active === false ? <IconButton icon={<FcApproval />} onClick={clickHandlerDisable}></IconButton> :
                     <IconButton icon={<FcCancel />} onClick={clickHandlerDisable}></IconButton>
